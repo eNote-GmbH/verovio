d5573e9ccb1ba9467ac83c19e979496d09e42486
Monceber <monceber.am@gmail.com>
2021-09-08 18:05:34 +0300

    Formatting fix

--- src/slur.cpp
+++ src/slur.cpp
@@ -747,7 +747,7 @@ std::pair<Point, Point> Slur::AdjustCoordinates(
                     else {
                         y2 = y1;
                         x2 += 2 * doc->GetDrawingStemWidth(staff->m_drawingStaffSize);
-                    }                    
+                    }
                 }
                 else {
                     y2 = end->GetDrawingTop(doc, staff->m_drawingStaffSize);
@@ -794,7 +794,7 @@ std::pair<Point, Point> Slur::AdjustCoordinates(
                     else {
                         y2 = y1;
                         x2 -= endRadius + doc->GetDrawingStemWidth(staff->m_drawingStaffSize);
-                    }                    
+                    }
                 }
                 else {
                     y2 = end->GetDrawingBottom(doc, staff->m_drawingStaffSize);
